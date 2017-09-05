30d3e14014908b74bfbe2d21341e6810f514b472
Dallas Caley <dallascaley@gmail.com>
2017-09-05 14:26:48 +0000

    first commit

---
+++ register.php
@@ -0,0 +1,27 @@
+<head>
+</head>
+<body>
+<form action="register_post">
+	<div>
+		<label>Username</label>
+		<input type="text" name="username"/>
+	</div>
+	<div>
+		<label>First Name</label>
+		<input type="text" name="first_name"/>
+	</div>
+	<div>
+		<label>Last Name</label>
+		<input type="text" name="last_name"/>
+	</div>
+	<div>
+		<label>Password</label>
+		<input type="password" name="pass"/>
+	</div>
+	<div>
+		<label>Password (verify)</label>
+		<input type="password" name="pass_verify"/>
+	</div>
+	<input type="submit" value="Submit"/>
+</form>
+</body>
