b2cdcb6409afce080e3f83964b8d8a0528b8605c
Dallas Caley <dallascaley@gmail.com>
2017-11-11 20:35:46 +0000

    WIP adding generic transactions interface

---
+++ js/add_transaction.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+
+	$('#transaction-form').validate({
+		rules: {
+			transaction: {
+				required: true,
+				currency: ["$", false],
+			},
+			transaction_frequency: {notNone: true}
+		}
+	});
+
+	$('#transaction-form').on('submit', function(e) {
+		e.preventDefault();
+
+		var params = getFormParams('#transaction-form');
+
+		var post_params = {
+			name: 'Test',
+			amount: params.transaction,
+			frequency: params.transaction_frequency,
+			days:[],
+			action: 'debit'
+		};
+
+		if ($('#transaction_frequency_weekday').is(':visible')) {
+			post_params.days.push($('#transaction_frequency_weekday').val());
+		}
+
+		if ($('#transaction_frequency_date').is(':visible')) {
+			post_params.date = $('#transaction_frequency_date').val();
+		}
+
+		if ($('#transaction_frequency_day_1').is(':visible')) {
+			post_params.days.push($('#transaction_frequency_day_1').val());
+		}
+
+		if ($('#transaction_frequency_day_2').is(':visible')) {
+			post_params.days.push($('#transaction_frequency_day_2').val());
+		}
+
+		$.post('/api/reoccurrence', post_params, function(response) {
+			if (response.status == 'Success') {
+				alert('Thank you');
+			};
+		},'json');
+
+	});
+});
